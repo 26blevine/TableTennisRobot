1a8471653152e38e0f25a21182dccc6e4837da3b
26blevine <26blevine@gannacademy.org>
2023-12-30 13:17:03 -0500

    test

--- groundTest_1.py
+++ groundTest_1.py
@@ -2,35 +2,37 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-
+# Left front
 in1_left = 17
 in2_left = 27
 ena_left = 22
+# Left back
 in3_left = 5
 in4_left = 6
 enb_left = 13
 #temp_left = 1
 
+# Right back
 in1_right = 23
 in2_right = 24
 ena_right = 25
+# Right front
 in3_right = 12
 in4_right = 16
 enb_right = 37
 #temp_right = 1
+
 outputPins = [17, 27, 22, 5, 6, 13, 23, 24, 25, 12, 16, 37]
 inPins = [17, 27, 5, 6, 23, 24, 12, 16]
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(outputPins, GPIO.OUT)
-
 
 a_left = GPIO.PWM(ena_left, 1000)
 b_left = GPIO.PWM(enb_left, 1000)
-
 a_right = GPIO.PWM(ena_right, 1000)
 b_right = GPIO.PWM(enb_right, 1000)
+
 a_left.start(75)
 b_left.start(75)
 a_right.start(75)
