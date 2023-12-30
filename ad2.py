421257f3847268e400ccc456b0be0fbe3063a0ec
26blevine <26blevine@gannacademy.org>
2023-12-30 15:53:39 -0500

    test

--- ad2.py
+++ ad2.py
@@ -4,18 +4,18 @@
 
 import RPi.GPIO as GPIO          
 from time import sleep
-
+import sys
 in1 = 24
 in2 = 23
-en = 25
+en = 26
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(12,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT)
-GPIO.setup(26,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
+GPIO.output(12,GPIO.LOW)
 GPIO.output(16,GPIO.LOW)
-GPIO.output(26,GPIO.LOW)
 
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
@@ -25,3 +25,5 @@ print("\n")
 GPIO.output(16, GPIO.HIGH)
 GPIO.output(26, GPIO.HIGH)
 sleep(10)
+GPIO.cleanup()
+sys.exit()
