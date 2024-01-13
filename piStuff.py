4128a490ee0822dc3eb4d50a925b5f8c98689f1c
26blevine <124911537+26blevine@users.noreply.github.com>
2024-01-12 22:42:26 -0500

    Add files via upload

--- piStuff.py
+++ piStuff.py
@@ -1,7 +1,10 @@
 import requests
+import subprocess
+
+subprocess.Popen('raspistill -n -o ~/Desktop/frame.jpg', shell=True)
 
 url = 'http://192.168.1.27:5000/api/send'  # Replace with the correct URL if needed
-files = {'file': open('.~/Desktop/frame.jpg', 'rb')}  # Replace with your file path
+files = {'file': open('   frame.jpg', 'rb')}  # Replace with your file path
 
 response = requests.post(url, files=files)
 print(response.json())
