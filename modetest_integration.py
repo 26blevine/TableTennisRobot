74f66cf0930d016040c12ad80e79fe80ff804557
26blevine <26blevine@gannacademy.org>
2024-05-12 19:12:52 -0400

    pushing

---
+++ modetest_integration.py
@@ -0,0 +1,75 @@
+import math
+import cv2
+from roboflow import Roboflow
+
+class objectBox:
+    def __init__(this, t, b, l, r):
+        this.top = t
+        this.bottom = b
+        this.left = l
+        this.right = r
+
+        this.x_center = (this.left+this.right)/2
+        this.y_center = (this.top+this.bottom)/2
+
+        this.x_dist = this.x_center - 320
+        this.y_dist = 480-this.y_center
+        this.absolute_distance = math.hypot(this.x_dist, this.y_dist)
+    def __repr__(this):
+        return f"abs {this.absolute_distance}, top {this.top}, bottom {this.bottom}, left {this.left}, right {this.right}"
+    def getAbsoluteDistance(this):
+        return this.absolute_distance
+# Roboflow Model Importing
+rf = Roboflow(api_key="iyABRjJ0CKFV2IT0liyK")
+project = rf.workspace("bsl").project("table-tennis-ball-model")
+dataset = project.version(1).download("yolov5")
+model = project.version(1).model
+# Video Capture Setup
+webcam = cv2.VideoCapture(0)
+webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cv2.namedWindow("Live View", cv2.WINDOW_NORMAL)
+
+leftMotors = 1
+rightMotors = 1
+
+print("Setup Complete!")
+while True:
+    # Take Frame From Webcam
+    print("New Frame")
+    result, frame = webcam.read()
+    # Choose Frame To Investigate
+    imageCaptured = frame
+    # Predict
+    prediction = model.predict(imageCaptured)
+    #Initialize list
+    ballsFound = []
+    # Calculate boundaries for bounding box
+    for bounding_box in prediction:
+        x1 = bounding_box['x'] - bounding_box['width'] / 2
+        x2 = bounding_box['x'] + bounding_box['width'] / 2
+        y1 = bounding_box['y'] - bounding_box['height'] / 2
+        y2 = bounding_box['y'] + bounding_box['height'] / 2
+        # Create bounding box object
+        ballBox = objectBox(x1, x2, y1, y2)
+        #add bounding box to list
+        ballsFound.append(ballBox)
+        #print(ballBox.absolute_distance)
+    #print("Unsorted:")
+    #print(ballsFound)
+    ballsFound.sort(key=lambda objectBox: objectBox.absolute_distance)
+    #print("Sorted")
+    #print(ballsFound)
+    prediction.plot()
+    closestBall = ballsFound[0]
+    print(f"The closest ball is {closestBall.absolute_distance} pixels away")
+
+    if closestBall.x_dist > 25:
+        leftMotors = 1
+        rightMotors = 0
+    elif closestBall.x_dist < -25:
+        leftMotors = 0
+        rightMotors = 1
+    else:
+        leftMotors = 1
+        rightMotors = 1
